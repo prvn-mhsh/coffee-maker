aaa9e361aaa2f363e0fdf3feec2dad812e7fd78f
Praveen M <98899001+prvn-mhsh@users.noreply.github.com>
2024-02-26 18:50:12 +0530

    Create cofeemaker.py

---
+++ cofeemaker.py
@@ -0,0 +1,39 @@
+//Materials Needed
+def materials():
+  materials = ['Coffee', 'Milk', 'Stove', 'Spoon', 'Pan', 'Sugar', 'Glass', 'Water']
+
+//Steps
+def get_the_milk():
+  print("Get the milk")
+def pour_milk_into_pan():
+  print("Pour the milk to pan");
+def mix_water():
+    print("Mix a small amount of water")
+def turn_on_stove():
+    print("Turn on the Stove")
+def wait_for_boil():
+    print("Wait for the Milk to boil")
+  def add_coffee_powder():
+    print("Add Coffee powder")
+def add_sugar():
+    print("Add Sugar")
+def stir_gently():
+    print("Stir gently")
+def pour_into_glasses():
+    print(" Pour into two glasses")
+def serve():
+    print("Serve before it cools down")
+  def prepare_coffee():
+
+    //Do Coffee
+    materials = get_materials()
+    get_milk()
+    pour_milk_into_pan()
+    mix_water()
+    turn_on_stove()
+    wait_for_boil()
+    add_coffee_powder()
+    add_sugar()
+    stir_gently()
+    pour_into_glasses()
+    serve()
